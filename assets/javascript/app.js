b4cd944ec97dcd083c0d72d6948c0d8810bdd2e5
travelingdoodle <reggiebrown@gmail.com>
2017-08-03 18:30:44 -0500

    fix append add empty

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     // ----------------------------Gif Tastic BS----------------------------
+    //-----------This accesses the GIPHY API ------------
+    //----------------(NOT UPDATED)-----------------
     $("#gif-button").on("click", function() {
 
       // Storing our giphy API URL for a random cat image
-      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=doggos";
+      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=doggo";
 
       // Perfoming an AJAX GET request to our queryURL
       $.ajax({
@@ -22,12 +24,122 @@ $(document).ready(function () {
 
         // Setting the catImage src attribute to imageUrl
         catImage.attr("src", imageUrl);
-        catImage.attr("alt", "cat image");
+        catImage.attr("alt", "GIPHY IMAGE");
 
         // Prepending the catImage to the images div
         $("#images").prepend(catImage);
+        console.log(response.rating);
       });
     });
+// ---------------- MOVIE-WORKING-APP--SOLVED ------------------
+//------------------(NOT UPDATED)-------------------
+// LISTENS FOR BUTTON EVEN --- RENDERS BUTTONS ---ALLOWS ADDITIONAL BUTTONS TO BE MADE------
+    
+      // Initial array of movies
+      var movies = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+
+      // displayMovieInfo function re-renders the HTML to display the appropriate content
+      function displayMovieInfo() {
+
+        // TO-DO rename this to 'animal'
+        var movie = $(this).attr("data-name");
+        //TO-DO update the query URL
+        var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+
+          // Creating a div to hold the movie
+          var movieDiv = $("<div class='movie'>");
+
+          // Storing the rating data
+          var rating = response.Rated;
+
+          // Creating an element to have the rating displayed
+          var pOne = $("<p>").text("Rating: " + rating);
+
+          // Displaying the rating
+          movieDiv.append(pOne);
+
+          // Storing the release year
+          var released = response.Released;
+
+          // Creating an element to hold the release year
+          var pTwo = $("<p>").text("Released: " + released);
+
+          // Displaying the release year
+          movieDiv.append(pTwo);
+
+          // Storing the plot
+          var plot = response.Plot;
+
+          // Creating an element to hold the plot
+          var pThree = $("<p>").text("Plot: " + plot);
+
+          // Appending the plot
+          movieDiv.append(pThree);
+
+          // Retrieving the URL for the image
+          var imgURL = response.Poster;
+
+          // Creating an element to hold the image
+          var image = $("<img>").attr("src", imgURL);
+
+          // Appending the image
+          movieDiv.append(image);
+
+          // Putting the entire movie above the previous movies
+          $("#movies-view").prepend(movieDiv);
+        });
+
+      }
+
+      // Function for displaying movie data
+      function renderButtons() {
+
+        // Deleting the movies prior to adding new movies
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttons-view").empty();
+
+        // Looping through the array of movies
+        for (var i = 0; i < movies.length; i++) {
+
+          // Then dynamicaly generating buttons for each movie in the array
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding a class of movie to our button
+          a.addClass("movie");
+          // Adding a data-attribute
+          a.attr("data-name", movies[i]);
+          // Providing the initial button text
+          a.text(movies[i]);
+          // Adding the button to the buttons-view div
+          $("#buttons-view").append(a);
+        }
+      }
+
+      // This function handles events where a movie button is clicked
+      $("#add-movie").on("click", function(event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var movie = $("#movie-input").val().trim();
+
+        // Adding movie from the textbox to our array
+        movies.push(movie);
+
+        // Calling renderButtons which handles the processing of our movie array
+        renderButtons();
+      });
+
+      // Adding a click event listener to all elements with a class of "movie"
+      $(document).on("click", ".movie", displayMovieInfo);
+
+      // Calling the renderButtons function to display the intial buttons
+      renderButtons();
+   
 
 
 
---
+++ assets/javascript/app2.js
@@ -0,0 +1,195 @@
+$(document).ready(function() {
+
+    // ---------------- MOVIE-WORKING-APP--SOLVED ------------------
+    
+    //_-_-_-_-_-_-_-_DELETE 5 THROUGH 47 -_-_-_---_-_-_-_-_-_-_
+    //-----set image function-------
+    // function displayArcher (){
+    //     var newDiv = $("<div>");
+    //     var imgURL = "https://vignette4.wikia.nocookie.net/archer/images/f/fb/Cheryl_Tunt01.png/revision/latest/scale-to-width-down/1000?cb=20151013030824";
+    //     console.log(imgURL);
+    //     var imgAnimate = "http://vignette2.wikia.nocookie.net/archer/images/3/3f/Sterling_Archer_Standing_POSE.png/revision/latest?cb=20151123090437";
+    //     var image = $("<img>").attr({
+    //                 src: imgURL,
+    //                 id: "stupidTest",
+    //                 state:  "still",
+    //                 still: imgURL,
+    //                 animate: imgAnimate,
+    //             });
+    //     newDiv.append(image);
+    //     $("#images").html(image);
+    //     console.log(imgAnimate);
+    //     console.log
+    //     alert("things!");
+        
+    // }
+    // $(document).on("click", "body", function(){
+    //     // $("#images").empty();
+    //     displayArcher();
+    //     console.log("body click");
+    // });
+    function displayArcher(){
+        $("#images").html('<img id="stupidTest" height="100" width="100" src="https://vignette4.wikia.nocookie.net/archer/images/f/fb/Cheryl_Tunt01.png/revision/latest/scale-to-width-down/1000?cb=20151013030824" state="still" animate="http://vignette2.wikia.nocookie.net/archer/images/3/3f/Sterling_Archer_Standing_POSE.png/revision/latest?cb=20151123090437" still="https://vignette4.wikia.nocookie.net/archer/images/f/fb/Cheryl_Tunt01.png/revision/latest/scale-to-width-down/1000?cb=20151013030824">');
+    }
+    //----------Function to Pause Giphs ----------
+    $("#stupidTest").click(function () {
+        console.log("stupidtest click function");
+        // $("#images").empty();
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("state");
+        console.log(state);
+        console.log($(this).attr("animate"));
+        console.log($(this).attr("still"));
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("animate"));
+            $(this).attr("state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("still"));
+            $(this).attr("state", "still");
+        }
+    });
+
+    //------------------(UPDATED - NOT FINISHED)-------------------
+    // LISTENS FOR BUTTON EVEN --- RENDERS BUTTONS ---ALLOWS ADDITIONAL BUTTONS TO BE MADE------
+
+    // Initial array of animals
+    var animals = ["Doggos", "Gold fish", "Hippo", "ardvark", "giraffe", "snail", "orka", "sperm whale", "woofer", "pupper", "bumblebee", "horse"];
+    console.log(animals);
+    // displayGiphy function re-renders the HTML to display the appropriate content
+    function displayGiphy() {
+
+
+        var animal = $(this).attr("data-name");
+        //TO-DO update the query URL for 10 IMAGES
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        console.log(queryURL);
+
+        //------EXAMPLE URL FROM GIPHY ----
+        // http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5
+
+        // Creating an AJAX call for the specific animal button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            // console.log(this);
+            //------ Loops for 10 imgs ------
+            for (var i = 0; i < 10; i++) {
+                // Creating a div to hold the animals
+                var animalDiv = $("<div class='animal_gif'>");
+
+                // Storing the rating data
+                var rating = response.data[i].rating;
+                console.log(response.data[i].rating);
+                console.log(response.data[i].images.original.url);
+                console.log(response.data[i].images.original_still.url);
+
+                // Creating an element to have the rating displayed
+                var pOne = $("<h3>").text("Rating: " + rating);
+
+                // Displaying the rating
+                animalDiv.append(pOne);
+
+                // Retrieving the URL for the image
+                var imgURL = response.data[i].images.original_still.url;
+                var imgAnimate = response.data[i].images.original.url;
+                console.log("animate" + imgAnimate);
+                console.log("still" + imgURL);
+
+                var image = $("<img>").attr({
+                    src: imgURL,
+                    class: "giph",
+                    state: "still",
+                    still: imgURL,
+                    animate: imgAnimate,
+                    id: "image"+i,
+                })
+
+                //<iframe src="https://giphy.com/embed/l2Sq0aDdjWbqgGsGQ" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dogs-lookhuman-doggos-l2Sq0aDdjWbqgGsGQ">via GIPHY</a></p>
+
+                // Appending the image
+                animalDiv.append(image);
+
+                // Putting the entire image above the previous giphy image
+                $("#images").prepend(animalDiv);
+            }
+        });
+    }
+    
+
+
+    // ----------Function for displaying giph buttons----------
+    function renderButtons() {
+
+        // Deleting the animals prior to adding new giphy
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#animal-buttons").empty();
+
+        // Looping through the array of animals
+        for (var i = 0; i < animals.length; i++) {
+
+            // Then dynamicaly generating buttons for each animal in the array
+            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+            var a = $("<button>");
+            // Adding a class of animals to our button
+            a.addClass("animals btn btn-primary");
+            // Adding a data-attribute
+            a.attr("data-name", animals[i]);
+            // Adding a data-attribute
+            a.attr("id", "button");
+            // Providing the initial button text
+            a.text(animals[i]);
+            // Adding the button to the buttons-view div
+            $("#animal-buttons").append(a);
+        }
+    }
+
+    // ----------This function handles events where an animal button is clicked----------
+    $("#addAnimal").on("click", function (event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var newAnimal = $("#animal-input").val().trim();
+
+        // Adding animal from the textbox to our array
+        animals.push(newAnimal);
+
+        // Calling renderButtons which handles the processing of our animals array
+        renderButtons();
+    });
+
+    // Adding a click event listener to all elements with a class of "animals"
+    $(document).on("click", ".animals", displayGiphy);
+
+    // Calling the renderButtons function to display the intial buttons
+    renderButtons();
+
+    // ---------_Experiment to get clickfunction working ---------
+    $("animal_gif").click(function() {
+        alert("You clicked a WINDOW OMG!!!");
+        alert($(this).attr("class"));
+       
+
+    });
+    $(document).on("click", "#image9", function (){
+        console.log("img.giph clickfunction");
+        // $("#images").empty();
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("state");
+        console.log(state);
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("animate"));
+            $(this).attr("state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("still"));
+            $(this).attr("state", "still");
+        }
+    });
+
+///document.ready ending
+});
